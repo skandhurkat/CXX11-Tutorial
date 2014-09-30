20ed30d406abd3285d4120beb23221356e68aff4
Skand Hurkat <skandhurkat@gmail.com>
2014-09-30 13:40:46 -0400

    microcounter uses threads without mutexes

--- microcounter.cpp
+++ microcounter.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 
 unsigned int counter = 0;
 
@@ -8,8 +9,15 @@ void increment(void) {
 }
 
 int main(void) {
+
+	std::vector<std::thread> threads;
+
 	for(unsigned int i = 0; i < 8; i++) {
-		increment();
+		threads.emplace_back(increment);
+	}
+
+	for(auto& t: threads) {
+		t.join();
 	}
 
 	std::cout << counter << std::endl;
